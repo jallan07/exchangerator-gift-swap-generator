b755138999b9ba0778e3c89fd0d634fc18a0306d
Josh Allan <allan.josh07@gmail.com>
2020-12-09 14:06:52 -0500

    Added logic that appends the form with additional name fields when the user clicks on the add more names button.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
   const submitBtn = $('#nameSubmit');
+  const formContainer = $('#formContainer');
+  const addMoreBtn = $('#addMoreNames');
   const participants = [];
+  let count = 4;
 
-  // event listeners
+  // on "add more names" button click
+  addMoreBtn.on('click', () => {
+    count++;
+    console.log('test');
+    formContainer.append(`
+    <div class="form-group">
+      <input
+        class="form-control form-control-lg"
+        type="text"
+        placeholder="Enter name ${count}"
+        id="inputLarge"
+      />
+    </div>
+    `);
+  });
+
+  // on form submit
   submitBtn.on('click', function (e) {
     e.preventDefault();
     console.log('test');
